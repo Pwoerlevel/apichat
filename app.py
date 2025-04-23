3a94cfc4fbb5a8c3c68a4f214099b1afec0f3415
hahsim2444 <96821915+Pwoerlevel@users.noreply.github.com>
2025-04-24 00:50:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ async def chat(request: Request):
     
     # التحقق من وجود النص المطلوب
     if required_text not in user_text:
-        return PlainTextResponse("يرجى إضافة الطلب بالصيغة الصحيحة 404")
+        return PlainTextResponse("يرجى إضافة الطلب بالصيغة الصحيحة 404666404666664040")
     
     # تكوين الرابط الخارجي
     external_url = f"https://text.pollinations.ai/{user_text}"
